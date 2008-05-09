f1de7c8c2508225c49f964b01c7c4d7bb453100b
Amit Kapoor <amitkapoor@mindspring.com>
2008-05-09 22:23:45 +0000

    Few changes:
     * Removed ^M at the end of every line
     * Set eol-style to native to avoid above problem
     * Added Apache 2.0 style license
     * Assigned copyright to Fedora Commons
     * Added java docs to functions

--- core/src/main/java/org/fedoracommons/akubra/BlobStore.java
+++ core/src/main/java/org/fedoracommons/akubra/BlobStore.java
@@ -1,3 +1,24 @@
+/* $HeadURL:: http://gandalf.topazproject.org/svn/head/topaz/core/src/main/java/org/topa#$
+ * $Id: ClassMetadata.java 5434 2008-04-12 11:41:51Z ronald $
+ *
+ * Copyright (c) 2007-2008 by Fedora Commons Inc.
+ * http://www.fedoracommons.org
+ * 
+ * In collaboration with Topaz Inc.
+ * http://www.topazproject.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.fedoracommons.akubra;
 
 import java.util.List;
@@ -5,12 +26,27 @@ import java.util.List;
 import javax.transaction.TransactionManager;
 
 public interface BlobStore {
-    
+  /**
+   * Return the list of stores underlying this store
+   *
+   * @return list of underlying blob stores
+   */
   List<BlobStore> getBackingStores();
-  
-  void setBackingStores(List<BlobStore> stores)
-          throws UnsupportedOperationException, IllegalStateException;
-  
-  BlobStoreConnection openConnection(TransactionManager txmgr);
 
+  /**
+   * Set the list of back store
+   *
+   * @param stores the list of backing store
+   */
+  void setBackingStores(List<BlobStore> stores)
+    throws UnsupportedOperationException, IllegalStateException;
+
+  /**
+   * Open connection to the blob store
+   *
+   * @param txmgr the transaction manager that will manage the transactions
+   *
+   * @return the connection to the blob store
+   */
+  BlobStoreConnection openConnection(TransactionManager txmgr);
 }
--- core/src/main/java/org/fedoracommons/akubra/BlobStoreConnection.java
+++ core/src/main/java/org/fedoracommons/akubra/BlobStoreConnection.java
@@ -1,27 +1,70 @@
+/* $HeadURL:: http://gandalf.topazproject.org/svn/head/topaz/core/src/main/java/org/topa#$
+ * $Id: ClassMetadata.java 5434 2008-04-12 11:41:51Z ronald $
+ *
+ * Copyright (c) 2007-2008 by Fedora Commons Inc.
+ * http://www.fedoracommons.org
+ * 
+ * In collaboration with Topaz Inc.
+ * http://www.topazproject.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.fedoracommons.akubra;
 
 import java.net.URI;
 
 public interface BlobStoreConnection {
-    
+  /**
+   * Return the blob associated with the id
+   *
+   * @param blobId URI identifying the blob
+   *
+   * @return the content
+   */
   Content getBlob(URI blobId);
-   
+
   /**
    * Stores a blob with the given id.
    * 
-   * If a blob id is specified, but such a blob already exists, its content
-   * will be replaced.
+   * If a blob id is specified, but such a blob already exists, its content will be replaced.
    * 
    * @param blobId the blob id, or null.
    * @param content the content to store.
+   *
    * @return the blob locator-id.
    */
   URI putBlob(URI blobId, Content content);
 
+  /**
+   * Remove the blob from the store
+   *
+   * @param blobId URI identifying the blob
+   *
+   * @return URI identifying the deleted blob
+   */
   URI removeBlob(URI blobId);
-    
+
+  /**
+   * Get the locator associated with the id
+   *
+   * @param blobId URI identifying the blob
+   *
+   * @return URI identifying the location
+   */
   URI getBlobLocator(URI blobId);
 
+  /**
+   * Close the connection to the blob store
+   */
   void close();
-    
 }

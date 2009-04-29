656e03215b39bc1f31676e68f3ab73d437d1b8d4
Ronald Tschalär <ronald@innovation.ch>
2009-04-29 20:34:38 +0000

    Minor code formatting fix.

--- akubra-core/src/main/java/org/fedoracommons/akubra/Blob.java
+++ akubra-core/src/main/java/org/fedoracommons/akubra/Blob.java
@@ -132,6 +132,7 @@ public interface Blob {
    * @throws DuplicateBlobException if a blob with <var>blobId</var> already exists
    * @throws IOException if an error occurs while attempting the operation
    */
-  Blob moveTo(URI blobId, Map<String, String> hints) throws DuplicateBlobException, IOException, MissingBlobException,
-                                NullPointerException, IllegalArgumentException;
+  Blob moveTo(URI blobId, Map<String, String> hints)
+      throws DuplicateBlobException, IOException, MissingBlobException, NullPointerException,
+             IllegalArgumentException;
 }
